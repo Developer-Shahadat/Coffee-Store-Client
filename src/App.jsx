250999c9fd5b96a569a0cf63c1f83bbd56d01de8
Developer-Shahadat <shahadatbci95@gmail.com>
2024-01-15 02:40:08 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
 import CoffeeCard from "./components/CoffeeCard";
+import { useState } from "react";
 
 function App() {
- const allCoffeeData = useLoaderData();
+  const allCoffeeData = useLoaderData();
+  const [coffees, setCoffees] = useState(allCoffeeData);
   return (
     <div>
-      
-      <h1 className='text-4xl text-violet-600 text-center'>Coffee Store : {allCoffeeData.length}</h1>
+      <h1 className="text-4xl text-violet-600 text-center">
+        Coffee Store : {coffees.length}
+      </h1>
       <div className="grid md:grid-cols-2 gap-6 my-20 m-20">
-        {
-          allCoffeeData.map(item => <CoffeeCard key={item._id} coffee={item}></CoffeeCard>)
-        }
+        {coffees.map((item) => (
+          <CoffeeCard
+            key={item._id}
+            coffee={item}
+            coffees={coffees}
+            setCoffees={setCoffees}
+          ></CoffeeCard>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
